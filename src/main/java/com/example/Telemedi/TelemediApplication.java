fb856c20add2075e2dd0e4cb6be70e80a5843201
samarth-sachin <samarthpatil@yahoo.com>
2025-09-08 22:57:08 +0530

    initial commit

---
+++ src/main/java/com/example/Telemedi/TelemediApplication.java
@@ -0,0 +1,13 @@
+package com.example.Telemedi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelemediApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelemediApplication.class, args);
+	}
+
+}
